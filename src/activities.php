880c7221ac04ae6b00d4e5a5ce38d5d55a550c3a
DESKTOP-ABMMVR6\Aron <47663@gymnasium.ax>
2023-01-17 09:27:58 +0200

    Hämta alla aktiviteter fungerar och har testcases

--- src/activities.php
+++ src/activities.php
@@ -1,7 +1,7 @@
 <?php
 
 declare (strict_types=1);
-
+require_once __DIR__ .'/funktioner.php';
 /**
  * Läs av rutt-information och anropa funktion baserat på angiven rutt
  * @param Route $route Rutt-information
@@ -37,7 +37,20 @@ function activities(Route $route, array $postData): Response {
  * @return Response
  */
 function hamtaAlla(): Response {
-    return new Response("Hämta alla aktiviteter", 200);
+    //koppla mot databasen
+    $db=connectDb();
+    //hämta alla poster från talbellen
+    $resultat=$db->query("SELECT id, kategori FROM kategorier");
+    // lägga in poster i en array
+    $retur=[];
+    while($row=$resultat->fetch()){
+        $post=new stdClass();
+        $post->id=$row['id'];
+        $post->activity=$row['kategori'];
+        $retur[]=$post;
+    }
+    //returnera svaret
+    return new Response($retur, 200);
 }
 
 /**
